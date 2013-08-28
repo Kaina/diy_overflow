c55bd4aadfa551ec897d51333f8096410d7b047c
zachgersh <zdgman@gmail.com>
2013-08-28 13:31:32 -0500

    Initial commit of new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiyOverflow::Application.config.secret_token = '470da83337b94f1a9604c45a81272d37dc2234bf07ada989d7dc6be2da0de4fce94ecdc9701199e31bc85976efbccf7af7522deb4002f89845d6f8b6efc0278a'
