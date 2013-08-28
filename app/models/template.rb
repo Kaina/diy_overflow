f16a7476e6ca61c49caa53903df601172765b5bf
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 15:57:44 -0500

    template passes association testing

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,8 +1,7 @@
 class Template < ActiveRecord::Base
-  # belongs_to :parent,   foreign_key: :template_id
+  belongs_to :parent,   class_name: "Template", foreign_key: :parent_id
   belongs_to :author,   foreign_key: :user_id
-  has_many   :children, class_name: "Template",
-                        foreign_key: :template_id
+  has_many   :children, class_name: "Template", foreign_key: :parent_id
 
   validates_presence_of :content, :author
 end
--- spec/Models/template_spec.rb
+++ spec/Models/template_spec.rb
@@ -5,6 +5,10 @@ describe Template do
                                 content: "I can't bacon.",
                                 author: "A Pig") }
 
+  it { should belong_to(:author) }
+  it { should belong_to(:parent) }
+  it { should have_many(:children) }
+
   it { should validate_presence_of(:content) }
   it { should validate_presence_of(:author) }
 end
