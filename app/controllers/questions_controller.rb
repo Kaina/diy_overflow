a22b298fef8f2a1250a8b8e1df99e4e86835ffe6
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 18:29:53 -0500

    removed puts statements from questions_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,11 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    puts params
     question = Question.create(params[:question])
     redirect_to question_url(question)
   end
 
   def show
-    puts "GOT HERE< <> >"
     @question = Question.find(params[:id])
   end
 end
