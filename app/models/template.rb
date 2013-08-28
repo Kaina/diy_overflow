e4653c7833f21807fbf93d9ef48f876992ecb13e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 15:50:47 -0500

    test passes validate presence of

---
+++ app/models/template.rb
@@ -0,0 +1,8 @@
+class Template < ActiveRecord::Base
+  # belongs_to :parent,   foreign_key: :template_id
+  belongs_to :author,   foreign_key: :user_id
+  has_many   :children, class_name: "Template",
+                        foreign_key: :template_id
+
+  validates_presence_of :content, :author
+end
