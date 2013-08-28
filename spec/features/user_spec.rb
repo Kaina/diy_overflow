71a4f36f5b7719d8403c93c3e89d87b91988f6b0
zachgersh <zdgman@gmail.com>
2013-08-28 15:24:35 -0500

    Small change, wanted just signin not user_signin

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "User sign up or sign in" do
   context "when on the sign up page" do
     it "should have a sign in with twitter link" do
-      visit user_signin_path
+      visit signin_path
       click_link "Sign in with Twitter"
     end
   end
