2c2f619e1a3c013a9eaf8861195dea9bda026306
zachgersh <zdgman@gmail.com>
2013-08-28 18:30:51 -0500

    updates test and moved twitter button to home

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "User sign up or sign in" do
   context "when on index page and not in session" do
-    it "should have a sign in with twitter link" do
+    it "user can sign in with twitter" do
       visit root_path
       find(".twitter-button").click
     end
