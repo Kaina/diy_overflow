020a82f72023267e2a2ccf859673b89405d25c9c
Ned KY <nkaidayip@gmail.com>
2013-08-28 15:12:10 -0500

    changed test to visit root_path

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+require '../spec_helper.rb'
 
 feature 'User browsing the website' do
   context "on index page" do
   	it "sees a sign in button" do
-  	  visit "/"
+  	  visit root_path
   	  page.has_content?("Sign in")	
   	end
   end	
