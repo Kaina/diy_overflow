d269368fcf96b2991671fe63ae424fe5aeabb991
zachgersh <zdgman@gmail.com>
2013-08-28 15:04:39 -0500

    Test added for just the button

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "User sign up or sign in" do
+  context "when on the sign up page" do
+    it "should have a sign in with twitter link" do
+      visit user_signin_path
+      click_link "Sign in with Twitter"
+    end
+  end
+end
