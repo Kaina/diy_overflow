85bcc1559e53ac4e20f4062194702fccd4b4741a
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-29 07:28:26 -0500

    added answer spec

---
+++ spec/features/answer_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'Answer Panel' do
+  context 'Create Answer' do
+    it 'can create an answer associated with a particular question and view it' do
+      question = Question.create(title: "How do I eat bacon?", content: "I'm really struggling with bacon" )
+
+      visit question_url(question)
+
+      click_link "Answer Question"
+
+      expect {
+        fill_in 'answer_content', with: "You put it in your mouth, man.  It's pretty simple."
+        click_button 'Save'
+      }.to change(Answer, :count).by 1
+
+      page.should have_content "You put it in your mouth, man.  It's pretty simple."
+    end
+  end
+end
