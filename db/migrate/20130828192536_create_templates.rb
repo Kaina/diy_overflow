7d7e5b8c98675b744913410d37d29e7eab828cdf
Ned KY <nkaidayip@gmail.com>
2013-08-29 14:54:27 -0500

    adds user foreign key to templates migration

--- db/migrate/20130828192536_create_templates.rb
+++ db/migrate/20130828192536_create_templates.rb
@@ -4,6 +4,7 @@ class CreateTemplates < ActiveRecord::Migration
       t.string     :title,   index: true
       t.text       :content, null: false
       t.integer    :parent_id
+      t.integer    :user_id
       t.timestamps
     end
   end
