02c6b37c31c03a390d3244055aab677f7bb31a70
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-29 18:20:24 -0500

    changes user BDD tests to give full coverage to session controller

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,13 +3,22 @@ require 'spec_helper'
 describe "User can auth" do
   context "when user signs in with twitter" do
     it "signs the user in and welcomes them" do
-      OmniAuth.config.add_mock :twitter, uid: "twitter-12345", info: { nickname: "Nedliest Catch", image: "imageofned.html" }, credentials: {token: "1234549039jrf0a", secret: "aoiefnaewofianefo"} 
-      visit '/auth/twitter'
+      OmniAuth.config.add_mock :twitter,
+                               uid: "twitter-12345",
+                               info: { nickname: "Nedliest Catch",
+                                       image: "imageofned.html" },
+                               credentials: { token: "1234549039jrf0a",
+                                              secret: "aoiefnaewofianefo" }
+      visit root_url
+      click_link "sign_in"
       page.should have_content "Welcome back Nedliest Catch"
+      page.should have_content('Logout')
     end
-    it "should display a logout link" do
+
+    it "signs the user out upon clicking log out" do
       visit '/auth/twitter'
-      page.should have_selector('a', text: 'Logout')
+      click_link "Logout"
+      page.should have_css("a#sign_in")
     end
   end
 end
