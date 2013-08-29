1ac8f563283b8c9afc5f537ca99f550545027945
zachgersh <zdgman@gmail.com>
2013-08-28 20:12:00 -0500

    moves Omniauth constant because it sucks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-OmniAuth.config.test_mode = true
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
@@ -9,6 +8,8 @@ require 'database_cleaner'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+OmniAuth.config.test_mode = true
 
 RSpec.configure do |config|
   config.include Capybara::DSL
