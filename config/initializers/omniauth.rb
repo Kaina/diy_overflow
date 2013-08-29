b80955e21cc13bbf75964dc8485a2f768546e914
zachgersh <zdgman@gmail.com>
2013-08-29 16:29:34 -0500

    adds twitter env variables instead of yaml

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-TWITTER_CONFIG = YAML.load_file("#{Rails.root}/config/twitter.yml")[Rails.env]
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, TWITTER_CONFIG["twitter_key"], TWITTER_CONFIG["twitter_secret"]
+  provider :twitter, ENV["twitter_key"], ENV["twitter_secret"]
 end
