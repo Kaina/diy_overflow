40ac4e55ff8755b63f945b7a9177f596080b8574
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 07:53:20 -0500

    forces user to log in to create an answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,13 @@
 class AnswersController < ApplicationController
 	def create
     question = Question.find(params[:answer][:question_id])
-		question.answers.create(content: params[:answer][:content])
-		redirect_to question_url(question)
+
+    if current_user
+  		question.answers.create(content: params[:answer][:content], user: current_user)
+    else
+      flash[:notice] = "Please Log In To Create An Answer"
+    end
+
+    redirect_to question_url(question)
 	end
 end
